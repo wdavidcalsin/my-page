643c0ba33bf1687622a115bfc07aabdf89842a63
Yony Calsin <helloyonicb@gmail.com>
2020-10-15 19:32:40 -0500

    feat: Create scripts (#7)
    * refactor: 💡 git fetch script was creaed !
    
    * refactor: 💡 fetch script was created !
    
    * refactor: 💡 eslint was disabled for service worker
    
    * perf: ⚡️ all the files were renamed to typescript

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,3 +1,5 @@
+/* eslint no-console: "off" */
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -20,7 +22,12 @@ const isLocalhost = Boolean(
       ),
 );
 
-export function register(config) {
+type Config = {
+   onSuccess?: (registration: ServiceWorkerRegistration) => void;
+   onUpdate?: (registration: ServiceWorkerRegistration) => void;
+};
+
+export function register(config?: Config) {
    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       // The URL constructor is available in all browsers that support SW.
       const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -54,7 +61,7 @@ export function register(config) {
    }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(swUrl: string, config?: Config) {
    navigator.serviceWorker
       .register(swUrl)
       .then((registration) => {
@@ -98,7 +105,7 @@ function registerValidSW(swUrl, config) {
       });
 }
 
-function checkValidServiceWorker(swUrl, config) {
+function checkValidServiceWorker(swUrl: string, config?: Config) {
    // Check if the service worker can be found. If it can't reload the page.
    fetch(swUrl, {
       headers: { 'Service-Worker': 'script' },
