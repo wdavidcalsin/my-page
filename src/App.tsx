898b9342e2b555a3f16d449ac66a2aabc81180f0
Wdavid Calsin <david2c142@gmail.com>
2020-10-23 19:23:09 -0500

    foot add abd twitter widget in section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Section from './components/Section/Section';
-
 function App() {
    return (
       <div>
          <Header />
          <Section />
+         <Footer />
       </div>
    );
 }
